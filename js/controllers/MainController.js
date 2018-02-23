342ed2ab66f188a301c047b65a4f00c0a98156e1
JovsQ <jovel@ideyatech.com>
2018-02-23 19:09:44 +0800

    DEV: Pollutants and percentages

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,13 +1,53 @@
 app.controller('MainController', ['$scope', function($scope){
 
+	$scope.years = [];
 	$scope.regions = [];
+	$scope.pollutants = {};
 
 	$scope.init = function(){
 		console.log('main controller init');
-		for (var i = 0; i < 10; i++){
-			$scope.regions.push(i);
-		}
-		console.log('regions length', $scope.regions.length);
+
+		mockValues();
 
 	};
+
+	var mockValues = function(){
+		$scope.regions = mockRegions(10);
+		$scope.pollutants.station = mockPollutants(12);
+		$scope.pollutants.mobile = mockPollutants(12);
+		$scope.pollutants.area = mockPollutants(12);
+	};
+
+	var mockRegions = function(numberOfMockItems){
+		var mockRegions = [];
+
+		for (var i = 0; i < numberOfMockItems; i++){
+			mockRegions.push(i);
+		}
+
+		return mockRegions;
+	}
+
+	var mockYears = function(numberOfMockItems){
+		var mockYears = [];
+
+		for (var i = 0; i < numberOfMockItems; i++){
+			mockYears.push(i);
+		}
+
+		return mockYears;
+	}
+
+	var mockPollutants = function(numberOfMockItems){
+		var mockPollutants = [];
+		for (var i = 0; i < numberOfMockItems; i++){
+			if (i % 2 == 0) {
+				mockPollutants.push(i);
+			} else {
+				mockPollutants.push('%');
+			}
+		}
+
+		return mockPollutants;
+	}
 }]);
