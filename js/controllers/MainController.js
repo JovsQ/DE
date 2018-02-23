d290710b16c36aa469b9be9d80c8077781352391
JovsQ <jovel@ideyatech.com>
2018-02-23 17:54:56 +0800

    DEV: Rows and columns

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,13 @@
 app.controller('MainController', ['$scope', function($scope){
 
+	$scope.regions = [];
+
 	$scope.init = function(){
 		console.log('main controller init');
+		for (var i = 0; i < 10; i++){
+			$scope.regions.push(i);
+		}
+		console.log('regions length', $scope.regions.length);
+
 	};
 }]);
